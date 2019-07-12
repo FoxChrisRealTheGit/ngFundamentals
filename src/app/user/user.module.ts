b4749f4373b1e26eff34613316e2f8c883cf706c
Christopher Fox <cfox@xpanxion.com>
2019-07-12 18:01:16 -0400

    reactive form started

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { LoginComponent } from "./login.component";
 import { ProfileComponent } from "./profile.component";
@@ -9,7 +9,12 @@ import { ProfileComponent } from "./profile.component";
 import { userRoutes } from "./user.routes";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, RouterModule.forChild(userRoutes)],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(userRoutes)
+  ],
   declarations: [ProfileComponent, LoginComponent],
   providers: []
 })
